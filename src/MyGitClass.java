6a3304a81b5adc6ac1b697926b80dfb64d76f1b3
hkilicaslan-sdet <hkilicaslansdet@gmail.com>
2022-07-16 22:47:36 +0300

    Third update

--- src/MyGitClass.java
+++ src/MyGitClass.java
@@ -2,5 +2,9 @@ public class MyGitClass {
     public static void main(String[] args) {
         System.out.println("Version 1");
         System.out.println("I am learning GitHub");
+        sayHello();
+    }
+    public static void sayHello(){
+        return;
     }
 }
